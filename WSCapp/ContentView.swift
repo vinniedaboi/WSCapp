b5c1b35f465cb92784f11662885f9904d440e128
vinniedaboi <kidosol5224@gmail.com>
2022-10-18 20:58:52 +0800

    Initial Commit

---
+++ WSCapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WSCapp
+//
+//  Created by vincent ng soon zheng  on 18/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WSCapp/WSCappApp.swift
@@ -0,0 +1,17 @@
+//
+//  WSCappApp.swift
+//  WSCapp
+//
+//  Created by vincent ng soon zheng  on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WSCappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
